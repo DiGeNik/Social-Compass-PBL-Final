ef1e57a7e69fb90f80d1235ece369e3c56bcc523
DiGeNik <128228683+DiGeNik@users.noreply.github.com>
2026-01-11 17:21:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE_NAME = 'crusoe-compass-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.webmanifest',
-  'https://cdn.tailwindcss.com'
+const CACHE_NAME = 'social-compass-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
+// Инсталиране и кеширане
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Кеширане на файловете');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
   );
 });
 
+// Използване на кеша (Offline first)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        // Връщаме кеширания файл, ако го има, иначе теглим от мрежата
+        return response || fetch(event.request);
+      })
   );
 });
